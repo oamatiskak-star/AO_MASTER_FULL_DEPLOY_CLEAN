5e2b0f3c06983854d08d58b101d6f99bc46137d1
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-09 22:42:07 +0100

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -4,27 +4,21 @@ dotenv.config()
 import express from "express"
 import cors from "cors"
 
-import projectsRoute from "./routes/projects.js"
 import executorRoute from "./routes/executor.js"
+import modulesRoute from "./routes/modules.js"
+import projectsRoute from "./routes/projects.js"
+import pingRoute from "./routes/ping.js"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// PROJECTS
-app.use("/api/projects", projectsRoute)
-
-// EXECUTOR
 app.use("/api/executor", executorRoute)
-
-// PING
-app.get("/api/ping", (req, res) => {
-res.json({ ok: true })
-})
+app.use("/api/modules", modulesRoute)
+app.use("/api/projects", projectsRoute)
+app.use("/api/ping", pingRoute)
 
 const PORT = process.env.PORT || 4000
-
 app.listen(PORT, () => {
-console.log("AO MASTER BACKEND draait op http://localhost
-:" + PORT)
+  console.log("AO MASTER BACKEND draait op http://localhost:" + PORT)
 })

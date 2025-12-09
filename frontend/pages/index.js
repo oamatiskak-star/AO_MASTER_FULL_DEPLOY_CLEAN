d6f963e7aec1596803cea6f6420db3759e223529
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-09 22:57:25 +0100

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import TaskButton from "../components/TaskButton"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>AO SaaS Dashboard</h1>
+      <TaskButton label="Test AO taak" task="test_taak" />
+    </div>
+  )
+}

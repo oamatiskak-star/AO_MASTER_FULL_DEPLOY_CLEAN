cea4b9c90da004abe8b67a949cd23ddd4d3efe77
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-10 23:12:48 +0100

    Create projectController.js

---
+++ backend-cloud/api/controllers/projectController.js
@@ -0,0 +1,9 @@
+module.exports = {
+  getAll: (req, res) => {
+    res.json({ ok: true, projects: [] })
+  },
+
+  create: (req, res) => {
+    res.json({ ok: true, created: req.body || {} })
+  }
+}

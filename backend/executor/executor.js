3d87696ed1d9e558b877c288849a24288cc00204
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-10 21:41:30 +0100

    Update executor.js

--- backend/executor/executor.js
+++ backend/executor/executor.js
@@ -55,5 +55,6 @@ console.log("Executor draait op poort " + PORT)
 
 // Local heartbeat ping to avoid Render HTML responses
 setInterval(() => {
-fetch(http://localhost:${PORT}/ping).catch(() => {})
+fetch(http://localhost:${PORT}/ping)
+.catch(() => {})
 }, 60000)

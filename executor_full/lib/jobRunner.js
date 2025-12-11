d228e82d935100a8d81ce8293f836987b3189ab3
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-11 18:46:14 +0100

    Update jobRunner.js

--- executor_full/lib/jobRunner.js
+++ executor_full/lib/jobRunner.js
@@ -1,8 +1,13 @@
+export async function runJob(job) {
+  console.log("Start job:", job.name)
 
-export async function runJob(job){
-  try{
-    await job.run()
-  } catch(e){
-    console.error("Fout in job:", job.name, e)
+  const log = (msg) => console.log(`[${job.name}] ${msg}`)
+
+  try {
+    await job.run(log)
+  } catch (err) {
+    console.log("Fout in job:", job.name, err)
   }
+
+  console.log("Klaar:", job.name)
 }

eb09c354b239c874e1e1aa37f8ab26cb39487ace
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-11 17:45:53 +0100

    Create job_build_main.js

---
+++ executor_full/jobs/job_build_main.js
@@ -0,0 +1,14 @@
+module.exports = {
+name: "job_build_main",
+
+async run({ log }) {
+log("BUILD MAIN job gestart")
+
+// Hier kan je straks echte build triggers plaatsen
+log("Main branch build flow uitgevoerd")
+
+return true
+
+
+}
+}

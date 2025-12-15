3ad17f3559143f8f368173728aa387e26ae6fe3b
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-15 11:30:55 +0100

    Update render-start.js

--- render-start.js
+++ render-start.js
@@ -2,12 +2,22 @@ import { spawn } from "child_process";
 
 console.log("AO Render start wrapper actief");
 
-const child = spawn("node", ["ao.js"], {
-  stdio: "inherit",
-  env: process.env,
+const child = spawn("node", ["src/ao.js"], {
+stdio: "inherit",
+env: process.env,
 });
 
 child.on("exit", (code) => {
-  console.log("AO exited with code", code);
-  process.exit(code);
+console.log("AO exited with code", code);
+process.exit(code);
 });
+
+package.json
+
+{
+"name": "ao-master-builder",
+"type": "module",
+"scripts": {
+"start": "node render-start.js"
+}
+}

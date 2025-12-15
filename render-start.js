fa37bff3517d1e6ac168d84e86c97834ce46176f
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-15 11:41:07 +0100

    Update render-start.js

--- render-start.js
+++ render-start.js
@@ -1,8 +1,16 @@
+const child = spawn("node", ["src/ao.js"], {
+
+Om naar:
+
+const child = spawn("node", ["ao.js"], {
+
+Volledige render-start.js:
+
 import { spawn } from "child_process";
 
 console.log("AO Render start wrapper actief");
 
-const child = spawn("node", ["src/ao.js"], {
+const child = spawn("node", ["ao.js"], {
 stdio: "inherit",
 env: process.env,
 });
